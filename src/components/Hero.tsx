8806523c08fe87c95c171da077ed64c39b3571fa
郑景东 <1572865506qq@gmail.com>
2026-04-15 14:57:49 +0000

    我搞错了文件名，应该是whiteboard02.png

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export function Hero({ locale }: { locale: Locale }) {
       {/* Background Image with Overlay */}
       <div className="absolute inset-0 z-0">
         <Image
-          src="/video/whiteboard01.png"
+          src="/video/whiteboard02.png"
           alt="Heovose Hero Background"
           fill
           className="object-cover opacity-60"
